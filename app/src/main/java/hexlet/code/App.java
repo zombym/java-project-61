e1c89bf2f6df3b8a0c34ede258e47a6cddff3d4a
Alexander <alexander.ponomarev@sephora.com>
2022-11-30 20:43:21 +0400

    task 9

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,9 +1,6 @@
 package hexlet.code;
 
-import hexlet.code.game.Calc;
-import hexlet.code.game.Even;
-import hexlet.code.game.GCD;
-import hexlet.code.game.Progression;
+import hexlet.code.game.*;
 
 public class App {
     public static void main(String[] args) {
@@ -13,6 +10,7 @@ public class App {
         System.out.println("3 - Calc");
         System.out.println("4 - GCD");
         System.out.println("5 - Progression");
+        System.out.println("6 - Prime");
         System.out.println("0 - Exit");
         var choose = Engine.input("Your choice:");
         switch(choose) {
@@ -30,6 +28,9 @@ public class App {
                 break;
             case "5":
                 Progression.progressionGame();
+                break;
+            case "6":
+                Prime.primeGame();
                 break;
             default:
                 System.out.println("Bay");
---
+++ app/src/main/java/hexlet/code/game/Prime.java
@@ -0,0 +1,52 @@
+package hexlet.code.game;
+
+import hexlet.code.Engine;
+import hexlet.code.Greet;
+
+public class Prime {
+
+    static String isPrime(int num) {
+        if (num <= 1) {
+            return "no";
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return "no";
+            }
+        }
+        return "yes";
+    }
+
+    static boolean game(String userName) {
+        int number = Engine.random(1, 100);
+        System.out.println("Question: " + number);
+        var answer = Engine.input("Your answer:");
+
+        var result = isPrime(number);
+
+        if (result.equals(answer)) {
+            System.out.println("Correct!");
+            return true;
+        } else {
+            System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was '" + result + "'.");
+            System.out.println("Let's try again, " + userName);
+            return false;
+        }
+    }
+
+    public static void primeGame() {
+        var userName = Greet.greetings();
+        System.out.println("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
+
+        var count = 0;
+        while (count < 3) {
+            var result = game(userName);
+            if(result){
+                count++;
+            } else {
+                return;
+            }
+        }
+        System.out.println("Congratulations, " + userName);
+    }
+}
