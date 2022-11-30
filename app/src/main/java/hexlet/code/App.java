02a2ae64d82604ad16ba540d073d28fd98601b35
Alexander <alexander.ponomarev@sephora.com>
2022-11-30 20:32:29 +0400

    task 8

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,6 +3,7 @@ package hexlet.code;
 import hexlet.code.game.Calc;
 import hexlet.code.game.Even;
 import hexlet.code.game.GCD;
+import hexlet.code.game.Progression;
 
 public class App {
     public static void main(String[] args) {
@@ -11,6 +12,7 @@ public class App {
         System.out.println("2 - Even");
         System.out.println("3 - Calc");
         System.out.println("4 - GCD");
+        System.out.println("5 - Progression");
         System.out.println("0 - Exit");
         var choose = Engine.input("Your choice:");
         switch(choose) {
@@ -25,6 +27,9 @@ public class App {
                 break;
             case "4":
                 GCD.GCDGame();
+                break;
+            case "5":
+                Progression.progressionGame();
                 break;
             default:
                 System.out.println("Bay");
---
+++ app/src/main/java/hexlet/code/game/Progression.java
@@ -0,0 +1,57 @@
+package hexlet.code.game;
+
+import hexlet.code.Engine;
+import hexlet.code.Greet;
+import java.util.Arrays;
+
+public class Progression {
+
+    static boolean game(String userName) {
+        int step = Engine.random(1, 5);
+        int len = Engine.random(5, 11);
+        int first = Engine.random(1, 50);
+        int unknown = Engine.random(0, len - 1);
+        int[] numbers = new int[len];
+        numbers[0] = first;
+        for (var i = 1; i < numbers.length; i++) {
+            numbers[i] = numbers[i-1] + step;
+        }
+        var result = 0;
+        for (var i = 0; i < numbers.length; i++) {
+            if(numbers[i] == numbers[unknown]){
+                System.out.print(".. ");
+                result = numbers[i];
+            } else {
+                System.out.print(numbers[i] + " ");
+            }
+        }
+
+        System.out.println();
+
+        var answer = Engine.input("Your answer:");
+        if (Integer.parseInt(answer) == result) {
+            System.out.println("Correct!");
+            return true;
+        } else {
+            System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was '" + result + "'.");
+            System.out.println("Let's try again, " + userName);
+            return false;
+        }
+    }
+
+    public static void progressionGame() {
+        var userName = Greet.greetings();
+        System.out.println("What number is missing in the progression?");
+
+        var count = 0;
+        while (count < 3) {
+            var result = game(userName);
+            if(result){
+                count++;
+            } else {
+                return;
+            }
+        }
+        System.out.println("Congratulations, " + userName);
+    }
+}
