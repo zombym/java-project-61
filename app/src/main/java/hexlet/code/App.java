2fb572beeec565ea4720e2016dfc7e41675adc83
Alexander <alexander.ponomarev@sephora.com>
2022-11-30 20:03:15 +0400

    task 7

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,6 +2,7 @@ package hexlet.code;
 
 import hexlet.code.game.Calc;
 import hexlet.code.game.Even;
+import hexlet.code.game.GCD;
 
 public class App {
     public static void main(String[] args) {
@@ -9,6 +10,7 @@ public class App {
         System.out.println("1 - Greet");
         System.out.println("2 - Even");
         System.out.println("3 - Calc");
+        System.out.println("4 - GCD");
         System.out.println("0 - Exit");
         var choose = Engine.input("Your choice:");
         switch(choose) {
@@ -20,6 +22,9 @@ public class App {
                 break;
             case "3":
                 Calc.calcGame();
+                break;
+            case "4":
+                GCD.GCDGame();
                 break;
             default:
                 System.out.println("Bay");
---
+++ app/src/main/java/hexlet/code/game/GCD.java
@@ -0,0 +1,50 @@
+package hexlet.code.game;
+
+import hexlet.code.Engine;
+import hexlet.code.Greet;
+
+public class GCD {
+
+    static int gcd(int a, int b) {
+        while (b != 0) {
+            int tmp = a % b;
+            a = b;
+            b = tmp;
+        }
+        return a;
+    }
+
+    static boolean game(String userName) {
+        int first = Engine.random(1, 100);
+        int second = Engine.random(1, 100);
+
+        System.out.println("Question: " + first + " " + second);
+        var answer = Engine.input("Your answer:");
+        var result = gcd(first, second);
+
+        if (Integer.parseInt(answer) == result) {
+            System.out.println("Correct!");
+            return true;
+        } else {
+            System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was '" + result + "'.");
+            System.out.println("Let's try again, " + userName);
+            return false;
+        }
+    }
+
+    public static void GCDGame() {
+        var userName = Greet.greetings();
+        System.out.println("Find the greatest common divisor of given numbers.");
+
+        var count = 0;
+        while (count < 3) {
+            var result = game(userName);
+            if(result){
+                count++;
+            } else {
+                return;
+            }
+        }
+        System.out.println("Congratulations, " + userName);
+    }
+}
