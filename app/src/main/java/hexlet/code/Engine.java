07e3c94291a2e5681ffd51d2b4acbec0a8875d65
Alexander <alexander.ponomarev@sephora.com>
2022-12-13 12:17:53 +0300

    separate_classes

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -1,17 +1,56 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class Engine {
+    static String input(String question) {
+        System.out.print(question + " ");
+        Scanner inputObj = new Scanner(System.in);
+        String answer = inputObj.nextLine();
+        return answer;
+    }
+
+    static void printCongratulation(String userName) {
+        System.out.println("Congratulations, " + userName + "!");
+    }
+
+    static void printTask(String task) {
+        System.out.println(task);
+    }
+
+    static void printQuestion(String question) {
+        System.out.println(question);
+    }
+
+    static String greetings() {
+        System.out.println("Welcome to the Brain Games!");
+        var userName = input("May I have your name?");
+        System.out.println("Hello, " + userName + "!");
+        return userName;
+    }
+
+    static boolean printAnswer(String answer, String result, String userName) {
+        if (answer.equals(result)) {
+            System.out.println("Correct!");
+            return true;
+        } else {
+            System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was '" + result + "'.");
+            System.out.println("Let's try again, " + userName + "!");
+            return false;
+        }
+    }
+
     public static void run(String task, String[][] roundsData) {
-        var userName = CommonTools.greetings();
+        var userName = greetings();
         for (var i = 0; i < CommonTools.REPEAT_COUNT; i++) {
-            CommonTools.printTask(task);
-            CommonTools.printQuestion(roundsData[i][0]);
-            var answer = CommonTools.input("Your answer:");
-            var result = CommonTools.printAnswer(answer, roundsData[i][1], userName);
+            printTask(task);
+            printQuestion(roundsData[i][0]);
+            var answer = input("Your answer:");
+            var result = printAnswer(answer, roundsData[i][1], userName);
             if (!result) {
                 return;
             }
         }
-        CommonTools.printCongratulation(userName);
+        printCongratulation(userName);
     }
 }
